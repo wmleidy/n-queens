c537992fc0e51cf4f8a3db70cb41be70d5fe044a
Bill Leidy <wmleidy@gmail.com>
2015-12-02 21:37:52 -0600

    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # n-queens
-A recursive solution to the classic n queens problem written in Ruby
+One Ruby solution to the classic programming problem of n queens, that is, find every way to arrange n queens on an n x n sized chessboard such that none of the queens are attacking each other along a file, rank, or diagonal. This solution prints each arrangement that satisfies these conditions (where 0 = the top-left corner, 1 = the square to the right of this, etc.) and also gives a count of the total number of arrangements.
+
+This was a fun one! I recommend you go and try to solve it right now.
---
+++ n_queens.rb
@@ -0,0 +1,46 @@
+def n_queens(n = 8, positions = [], count = 0, row = 0)
+	if violation?(n, positions)
+		return count
+	elsif positions.length == n
+		count += 1
+		p positions
+		return count
+	else
+		(0..n-1).to_a.each do |pos|
+			new_queen_location = pos + row * n
+			positions << new_queen_location
+			count = n_queens(n, positions, count, row + 1)
+			positions.pop
+		end
+	end
+	count
+end
+
+def violation?(n, positions)
+	x = []
+	y = []
+	forward_dia  = []
+	backward_dia = []
+	positions.each do |pos|
+		x << pos % n
+		y << pos / n
+		forward_dia  << pos % n - pos / n
+		backward_dia << pos % n + pos / n
+	end
+	if x != x.uniq || y != y.uniq || forward_dia != forward_dia.uniq || backward_dia != backward_dia.uniq
+		true
+	else
+		false
+	end
+end
+
+# 4 => 2
+# 5 => 10
+# 6 => 4
+# 7 => 40
+# 8 => 92
+# 9 => 352
+# 10 => 724
+# 11 => 2680
+# 12 => 14200
+# 13 => 73712
